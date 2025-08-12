f5b6b460ede3896a67e2ececac0e2529ec55ff91
DaDanny <danny.francken@bonus.ly>
2025-08-12 18:36:00 -0400

    Initial Commit

---
+++ HotHint/HotHintApp.swift
@@ -0,0 +1,32 @@
+//
+//  HotHintApp.swift
+//  HotHint
+//
+//  Created by Danny Francken on 8/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HotHintApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
